949c48b4ede013e74b9f4b60132a2f07bb3a029f
steveonlinux <stephenmarks5309@gmail.com>
2024-09-02 10:03:46 -0700

    aliasrc

--- link.sh
+++ link.sh
@@ -1,54 +1,39 @@
 #!/bin/sh
 
-# Create links to home
-ln -s bashrc "$HOME/.bashrc"
-ln -s zshrc "$HOME/.zshrc"
+# RUN IN SAME DIRECTORY AS DOTS
+
+# Get the absolute path to the directory where the script is located
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+# Function to create a symbolic link after removing existing files or symlinks
+create_symlink() {
+  TARGET="$1"
+  LINK_NAME="$2"
+
+  # Check if the link or file exists and remove it
+  if [ -e "$LINK_NAME" ] || [ -L "$LINK_NAME" ]; then
+    rm -rf "$LINK_NAME"
+  fi
+
+  # Create the symbolic link
+  ln -s "$TARGET" "$LINK_NAME"
+}
+
+# Create links to home using absolute paths
+create_symlink "$SCRIPT_DIR/bashrc" "$HOME/.bashrc"
+create_symlink "$SCRIPT_DIR/zshrc" "$HOME/.zshrc"
 
 # Create links to scripts
-ln -s scr/framework "$HOME/scr/framework"
+create_symlink "$SCRIPT_DIR/scr/framework" "$HOME/scr/framework"
 
-# Create links to .config
-#ln -s .config/[] "$HOME/.config[]"
-ln -s config/aliasrc "$HOME/.config/aliasrc"
-ln -s config/okularpartrc "$HOME/.config/okularpartrc"
-ln -s config/okularrc "$HOME/.config/okularrc"
-ln -s config/picom.conf "$HOME/.config/picom.conf"
+# Create links to .config using absolute paths
+create_symlink "$SCRIPT_DIR/config/aliasrc" "$HOME/.config/aliasrc"
+create_symlink "$SCRIPT_DIR/config/okularpartrc" "$HOME/.config/okularpartrc"
+create_symlink "$SCRIPT_DIR/config/okularrc" "$HOME/.config/okularrc"
+create_symlink "$SCRIPT_DIR/config/picom.conf" "$HOME/.config/picom.conf"
 
-ln -s config/awesome "$HOME/.config/awesome"
-ln -s config/fastfetch "$HOME/.config/fastfetch"
-ln -s config/hypr "$HOME/.config/hypr"
-ln -s config/kitty "$HOME/.config/kitty"
-ln -s config/leftwm "$HOME/.config/leftwm"
-ln -s config/lvim "$HOME/.config/lvim"
-ln -s config/mpv "$HOME/.config/mpv"
-ln -s config/neofetch "$HOME/.config/neofetch"
-ln -s config/qalculate "$HOME/.config/qalculate"
-ln -s config/qtile "$HOME/.config/qtile"
-ln -s config/ranger "$HOME/.config/ranger"
-ln -s config/sway "$HOME/.config/sway"
-ln -s config/waybar "$HOME/.config/waybar"
+# Link directories using absolute paths
+for dir in awesome fastfetch hypr kitty leftwm lvim mpv neofetch qalculate qtile ranger sway waybar; do
+  create_symlink "$SCRIPT_DIR/config/$dir" "$HOME/.config/$dir"
+done
 
-#create_symlinks() {
-#    src_dir="$1"  # Source directory provided as the argument
-#    dest_dir="$HOME/.config"  # Destination directory for symlinks
-#
-#    # Check if the source directory exists
-#    if [[ ! -d "$src_dir" ]]; then
-#        echo "Error: Source directory '$src_dir' does not exist."
-#        return 1
-#    fi
-#
-#    # Create the destination directory if it does not exist
-#    mkdir -p "$dest_dir"
-#
-#    # Iterate over all files and directories in the source directory
-#    for file in "$src_dir"/*; do
-#        # Get the base name of the file or directory
-#        basename=$(basename "$file")
-#        
-#        # Create a symbolic link
-#        ln -s "$file" "$dest_dir/$basename"
-#    done
-#
-#    echo "Symbolic links created in '$dest_dir'."
-#}
